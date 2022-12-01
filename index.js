b17f6cc6eda17c1ec1c080311950e3613db9e3e0
grace-kathure <gracegikundak@gmail.com>
2022-12-01 21:29:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    return customerName = "BOB"
+}
+
+bestCustomer 
+function setBestCustomer(){
+    return bestCustomer = "not bob"
+}
+setBestCustomer()
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob"
+}
+overwriteBestCustomer()
+
+const leastfavouriteCustomer = "mary"
+function changeLeastFavoriteCustomer(){
+    const leastfavouriteCustomer = "mary"
+    if(leastfavouriteCustomer === "mary"){
+        leastfavouriteCustomer = "emily"
+    }
+}
+changeLeastFavoriteCustomer()
